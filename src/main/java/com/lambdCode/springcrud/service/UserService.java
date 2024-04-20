82be16fb3e0f95ee1a8ebaa10ba932dbd2b0970f
Evanilson Pereira <pevanilson99@gmail.com>
2024-04-20 17:10:56 -0300

    Created Controller - Save a user method

--- src/main/java/com/lambdCode/springcrud/service/UserService.java
+++ src/main/java/com/lambdCode/springcrud/service/UserService.java
@@ -1,4 +1,7 @@
 package com.lambdCode.springcrud.service;
 
+import com.lambdCode.springcrud.entity.User;
+
 public interface UserService {
+    void addUser(User user);
 }
--- src/main/java/com/lambdCode/springcrud/service/impl/UserServiceImpl.java
+++ src/main/java/com/lambdCode/springcrud/service/impl/UserServiceImpl.java
@@ -1,8 +1,19 @@
 package com.lambdCode.springcrud.service.impl;
 
+import com.lambdCode.springcrud.entity.User;
+import com.lambdCode.springcrud.repository.UserRepository;
 import com.lambdCode.springcrud.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
 }
